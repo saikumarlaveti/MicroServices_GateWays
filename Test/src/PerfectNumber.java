9c402f193f709d82231d084f4913d21c89615c0a
saiku <saiku@DESKTOP-8SRB5RP>
2025-10-21 22:19:12 +0530

    IMP

--- Test/src/PerfectNumber.java
+++ Test/src/PerfectNumber.java
@@ -1,4 +1,21 @@
 
 public class PerfectNumber {
+	public boolean isPerfect(int n) {
+		int sum = 1;  // 1 is always a proper divisor
 
+		for (int i = 2; i * i <= n; i++) {
+		    if (n % i == 0) {
+		        sum += i;
+		        if (i != n / i) 
+		        	sum += n / i;
+		    }
+		}
+		return sum == n;
+	}
+	public static void main(String[] args) {
+		PerfectNumber pn = new PerfectNumber();
+		System.out.println(pn.isPerfect(6));
+		System.out.println(pn.isPerfect(12));
+		System.out.println("Saikumar Laveti");
+}
 }
