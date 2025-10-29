6b7e90d5f0c6c029735d52b4e45b07102f18aafa
saiku <saiku@DESKTOP-8SRB5RP>
2025-10-29 20:22:54 +0530

    Final

---
+++ MS-03-BillingServiceMS_Spring Cloud Gateway_Filters/src/main/java/com/example/demo/Ms03ShoppingServiceMs2LoggersApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Ms03ShoppingServiceMs2LoggersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ms03ShoppingServiceMs2LoggersApplication.class, args);
+	}
+}
+
+
